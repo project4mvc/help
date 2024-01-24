baeede88aab15f5af5740889bb0b4b2c953e86bc
Chris Bouma <chr1sje.bouma@gmail.com>
2024-01-24 12:03:58 +0100

    help

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'be-opdracht1-j2-p1');
+define('URLROOT', 'http://mazinjamil/Backend-Opdrachten/public');
+ 
